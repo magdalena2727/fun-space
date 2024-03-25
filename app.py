d75eb6df7ed370f6f87b3ddca031a2094aafd39e
Magdalena Pardosi <144409015+magdalena2727@users.noreply.github.com>
2024-03-25 22:09:41 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+#HITUNG LUAS SEGITIGA
+
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
+#HITUNG LUAS PERSEGI PANJANG
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi: "))
+    l = int(input("Masukkan lebar persegi: "))
+    luas = p * l
+    print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
